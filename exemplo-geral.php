5986f5d66ea11f83425652de38b379f0df05e682
Ignacio Neto <ignacio.cunha.neto@gmail.com>
2022-06-06 11:38:04 -0300

    Exemplo 1
    Criação de classe e objetos.

---
+++ exemplo-geral.php
@@ -0,0 +1,19 @@
+<?php
+// Exemplo geral
+
+// ____________________________________________________________________
+class Exemplo {
+    // Atributos ou propriedades
+    public string $nome;
+    public string $escola;
+    public string $cidade;
+}
+
+// Criação objetos/instâncias
+$exemploA = new Exemplo;
+$exemploB = new Exemplo;
+
+echo "<pre>";
+var_dump($exemploA);
+var_dump($exemploB);
+echo "<pre>";
